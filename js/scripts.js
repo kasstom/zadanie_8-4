84d087db1e4a0c4946fe8ad160eae2b63a6960cd
Aneta Koppernock <akozuchowska@hotmail.com>
2018-02-13 23:44:29 +0100

    Koniec zadania

---
+++ js/scripts.js
@@ -0,0 +1,12 @@
+// scripts.js
+
+var name = prompt('Enter your name');
+// komentarz do alerta
+alert('Hello, ' + name); // allert hello
+
+console.log('Hello, ' + name);
+/* 
+var name = prompt('Enter your name');
+alert('Hello, ' + name);
+console.log('Hello, ' + name);
+*/
